a322d8b15cbe21c2dbb0654c5b80ebc3a85979a8
tonic <tonicbupt@gmail.com>
2015-10-21 15:29:18 +0800

    ...

--- show_env.py
+++ show_env.py
@@ -6,5 +6,8 @@ for key, value in os.environ.iteritems():
     print >> sys.stderr, key, value
 
 while True:
-    time.sleep(100)
+    time.sleep(10)
     print >> sys.stderr, 'sleep, waiting for ending'
+
+    for key, value in os.environ.iteritems():
+        print >> sys.stderr, key, value
